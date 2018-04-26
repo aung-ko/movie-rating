a9df860cafe941617ce314383e0dd2f386944630
Lin Htet Paing <linhtetpaing9@gmail.com>
2018-04-26 13:33:16 +0630

    Movie Crud not including Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,56 +7,56 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 
-class Errors{
-	constructor(){
-		this.errors = {};
-	}
-	get(field){
-		if(this.errors[field]){
-			return this.errors[field][0];
-		}
-	}
-	record(errors){
-		this.errors = errors.errors;
-	}
-	clear(field){
-		delete this.errors[field];
-	}
-	has(field){
-		return this.errors.hasOwnProperty(field);
-	}
-	any(){
-		return Object.keys(this.errors).length > 0;
-	}
+// class Errors{
+// 	constructor(){
+// 		this.errors = {};
+// 	}
+// 	get(field){
+// 		if(this.errors[field]){
+// 			return this.errors[field][0];
+// 		}
+// 	}
+// 	record(errors){
+// 		this.errors = errors.errors;
+// 	}
+// 	clear(field){
+// 		delete this.errors[field];
+// 	}
+// 	has(field){
+// 		return this.errors.hasOwnProperty(field);
+// 	}
+// 	any(){
+// 		return Object.keys(this.errors).length > 0;
+// 	}
 
-}
+// }
 
-new Vue({
-    el: '#root',
-    data:{
-    	movie_name: '',
-    	director_name: '',
-    	description: '',
-    	genres: '',
-    	released_date: '',
-    	errors: new Errors()
-    },
-    methods:{
-    	onSubmit(){
-    		axios.post('/movie', this.$data)
-    		.then(this.onSuccess)
-    		.catch(error => this.errors.record(error.response.data));
-    	},
-    	onSuccess(response){
-    		alert('Success');
-    		this.movie_name = '';
-    		this.director_name = '';
-    		this.description = '';
-    		this.genres = '';
-    		this.released_date = '';
-    	}
-    }
-});
+// new Vue({
+//     el: '#root',
+//     data:{
+//     	movie_name: '',
+//     	director_name: '',
+//     	description: '',
+//     	genres: '',
+//     	released_date: '',
+//     	errors: new Errors()
+//     },
+//     methods:{
+//     	onSubmit(){
+//     		axios.post('/movie', this.$data)
+//     		.then(this.onSuccess)
+//     		.catch(error => this.errors.record(error.response.data));
+//     	},
+//     	onSuccess(response){
+//     		alert('Success');
+//     		this.movie_name = '';
+//     		this.director_name = '';
+//     		this.description = '';
+//     		this.genres = '';
+//     		this.released_date = '';
+//     	}
+//     }
+// });
--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -3,45 +3,45 @@
 @section('content')
 
 <div class="container" id="root">
-	<form action="{{route('movie.store')}}" method="POST" @submit.prevent="onSubmit" @click="errors.clear($event.target.name)">
+	<form action="{{route('movie.store')}}" method="POST">
 		{{csrf_field()}}
 		<div class="form-group">
 			<label for="movie_name">Movie Name</label>
-			<input type="text" name="movie_name" id="movie_name" class="form-control" v-model="movie_name">
+			<input type="text" name="movie_name" id="movie_name" class="form-control">
 			<br>
-			<div class="alert alert-danger" v-if="errors.has('movie_name')" v-text="errors.get('movie_name')"></div>
+			
 		</div>
 
 		<div class="form-group">
 			<label for="director_name">Director Name</label>
-			<input type="text" name="director_name" id="director_name" class="form-control" v-model="director_name">
+			<input type="text" name="director_name" id="director_name" class="form-control">
 			<br>
-			<div class="alert alert-danger" v-if="errors.has('director_name')" v-text="errors.get('director_name')"></div>
+			
 		</div>
 
 		<div class="form-group">
 			<label for="description">Description</label>
-			<input type="text" name="description" id="description" class="form-control" v-model="description">
+			<input type="text" name="description" id="description" class="form-control">
 			<br>
-			<div class="alert alert-danger" v-if="errors.has('description')" v-text="errors.get('description')"></div>
+			
 		</div>
 
 		<div class="form-group">
 			<label for="genres">Genres</label>
-			<input type="text" name="genres" id="genres" class="form-control" v-model="genres">
+			<input type="text" name="genres" id="genres" class="form-control">
 			<br>
-			<div class="alert alert-danger" v-if="errors.has('genres')" v-text="errors.get('genres')"></div>
+			
 		</div>
 
 
 		<div class="form-group">
 			<label for="released_date">Released Date</label>
-			<input type="date" name="released_date" id="released_date" class="form-control" v-model="released_date" >
+			<input type="date" name="released_date" id="released_date" class="form-control">
 			<br>
-			<div class="alert alert-danger" v-if="errors.has('released_date')" v-text="errors.get('released_date')"></div>
+			
 		</div>
 		<div class="form-group">
-			<button type="submit" class="btn btn-success" :disabled="errors.any()">Create</button>
+			<button type="submit" class="btn btn-success">Create</button>
 		</div>
 	</form>
 </div>
