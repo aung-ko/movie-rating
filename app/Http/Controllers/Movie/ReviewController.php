0119d505bc645b7f4b46fb231703737b7a02c0fe
Aung Ko <aungko@softhatch.com>
2018-06-24 18:19:07 +0630

    modified some

--- app/Http/Controllers/Movie/ReviewController.php
+++ app/Http/Controllers/Movie/ReviewController.php
@@ -3,38 +3,39 @@
 namespace App\Http\Controllers\Movie;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Movie;
 use App\Models\Review;
-use App\Models\Reply;
+use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-	public function create(Movie $movie)
-	{
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['store', 'update', 'destroy']);
+    }
 
-		return view('reviews.review-create', compact('movie'));
-	}
-    
-    
+    public function create(Movie $movie)
+    {
+        return view('reviews.review-create', compact('movie'));
+    }
+
     public function store(Request $request, Movie $movie)
     {
         $request->validate([
             'rating' => 'required',
             'title' => 'string',
-            'body' => 'string'
+            'body' => 'string',
         ]);
 
-
-        if(\Auth::check()){
-        Review::create([
-            'title' => $request->title,
-            'body' => $request->body,
-            'user_id' => \Auth::user()->id,
-            'movie_id' => $movie->id,
-            'rating' => $request->rating
-        ]);
-        }else{
+        if (\Auth::check()) {
+            Review::create([
+                'title' => $request->title,
+                'body' => $request->body,
+                'user_id' => \Auth::user()->id,
+                'movie_id' => $movie->id,
+                'rating' => $request->rating,
+            ]);
+        } else {
             abort(403, 'Unauthorized action. You have to login');
         }
 
@@ -48,40 +49,35 @@ class ReviewController extends Controller
         return view('reviews.review-show', compact('review', 'movie'));
     }
 
-   
-
-    public function update(Request $request,Movie $movie, Review $review)
+    public function update(Request $request, Movie $movie, Review $review)
     {
 
         $this->validate($request, [
             'title' => 'required|string',
             'rating' => 'required',
-            'body' => 'required|string'
+            'body' => 'required|string',
         ]);
 
+        if (\Auth::check()) {
+            $review->update([
+                'title' => $request->title,
+                'body' => $request->body,
+                'rating' => $request->rating,
+                'user_id' => \Auth::user()->id,
+                'movie_id' => $movie->id,
 
-        if(\Auth::check()){
-        $review->update([
-            'title' => $request->title,
-            'body' => $request->body,
-            'rating' => $request->rating,
-            'user_id' => \Auth::user()->id,
-            'movie_id' => $movie->id
-
-        ]);
-        }else{
+            ]);
+        } else {
             abort(403, 'Unauthorized action. You have to login');
         }
 
         return redirect()->route('movie.show', $movie->slug);
     }
 
-    
-
     public function destroy(Movie $movie, Review $review)
     {
         // dd($review);
-        $review = Review::where('id', '=' , $review->id)->delete();
+        $review = Review::where('id', '=', $review->id)->delete();
         return redirect()->route('movie.show', $movie->slug);
     }
 }
